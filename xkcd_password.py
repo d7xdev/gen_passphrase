a65810d79bb28ef46e6510ea7c0e3af09547f5bc
d7xdev <d7xdev@users.noreply.github.com>
2020-04-10 16:09:48 -0400

    refactor Python code
    - reduce code indentation
    - add EFF word lists URL source

--- xkcd_password.py
+++ xkcd_password.py
@@ -2,29 +2,34 @@
 
 # Source #1: https://docs.python.org/3/library/secrets.html#secrets.choice
 # Source #2: https://xkcd.com/936/
+# Source #3: https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases
 
 import secrets
-from collections import Counter
-from math import log
 import sys
+import math
 
 WORDLIST='dict/eff_short_wordlist_2_0.txt'
 PW_LEN=4
 
 with open(WORDLIST) as f:
     words = [line.strip().split()[1] for line in f]
-    wc = Counter(words)
-    if len(words) != len(wc):
-        sys.stderr.write(WORDLIST+": ERROR: duped word detected!\n")
-        sys.exit(1)
-    entropy     = log(len(wc))/log(2)
-    pw_entropy  = PW_LEN * entropy
-    password    = ' '.join(secrets.choice(words) for i in range(PW_LEN))
-    print('''
+
+## sanity check to make sure words are unique
+from collections import Counter
+wc = Counter(words)
+if len(words) != len(wc):
+    sys.stderr.write(WORDLIST+": ERROR: duped word detected!\n")
+    sys.exit(1)
+
+entropy     = math.log(len(words))/math.log(2)
+pw_entropy  = PW_LEN * entropy
+password    = ' '.join(secrets.choice(words) for i in range(PW_LEN))
+
+print('''
 Dictionary  : %s
 Dict Len    : %10d (word)
 Entropy     : %10.2f (bit/word)
 Pw Len      : %10d (word)
 Pw Entropy  : %10.2f (bit)
 Password    : %s
-''' % (WORDLIST, len(wc), entropy, PW_LEN, pw_entropy, password))
+''' % (WORDLIST, len(words), entropy, PW_LEN, pw_entropy, password))
